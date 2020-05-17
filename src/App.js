3ce5b90af56ec7c6e936c5ebbf3626ccb9c71bd4
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-05-17 23:45:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import Bouton from './components/Bouton';
 function App() {
   return (
     <div className="App">
-      <br></br><br></br>
+      <br />
         <Field />
         <Bouton />
-        <br></br><br></br>
+        <br />
         <Field />
         <Bouton />
-        <br></br><br></br>
-        
+        <br />    
     </div>
   );
 }
